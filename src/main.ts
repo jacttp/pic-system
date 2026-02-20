5384eb3ec8bf076d5955e0634a44b24d22b2d390
jaime.arias <jacttp@hotmail.com>
2026-02-20 12:55:56 -0600

    v1.15.3 Corrección Rutas Relativas en Leaflet

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,22 @@ import './utils/chartConfig';
 // FontAwesome (versión CDN o npm)
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
+// Leaflet Vite fixes
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+   iconRetinaUrl,
+   iconUrl,
+   shadowUrl,
+});
+
 const app = createApp(App)
 
 app.use(createPinia())
